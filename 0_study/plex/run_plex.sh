57fb3bbacbd5007c62c5da41c82a52166d38d056
tony <tonybutzer@gmail.com>
2024-12-17 11:11:16 -0600

    sleepy work 2022 November

--- 0_study/plex/run_plex.sh
+++ 0_study/plex/run_plex.sh
@@ -8,7 +8,8 @@ docker run -d \
   -e PLEX_CLAIM= `#optional` \
   -v /path/to/plex/library:/config \
   -v /path/to/tvseries:/tv \
-  -v /path/to/movies:/movies \
+  -v /media/tony/22980B44725E901B/0_Movies:/movies \
+  -v /media/tony/22980B44725E901B/0_Music:/music \
   --restart unless-stopped \
 docker.io/linuxserver/plex:latest
 
