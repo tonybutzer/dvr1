813942ad5c136fd4c7b3827411411250051442a1
tony <tonybutzer@gmail.com>
2025-01-21 12:41:42 -0600

    sleepy work 2022 November

--- 5_tv_guide/get_new_zap_sched.sh
+++ 5_tv_guide/get_new_zap_sched.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-(cd /home/tony/opt/dvr1/5_tv_guide ; python orig_zap2xml.py -z 57103)
+(cd /home/tony/opt/dvr1/5_tv_guide ; rm -f cache/*)
+(cd /home/tony/opt/dvr1/5_tv_guide ;  python orig_zap2xml.py -z 57103)
